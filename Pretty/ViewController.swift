a98e745dfb68f4be0431f6ce2b46572d218f6717
Anh Tuong Nguyen <anhtuongmr@gmail.com>
2017-04-21 23:05:32 +0700

    Initial Commit

---
+++ Pretty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pretty
+//
+//  Created by Anh Tuong Nguyen on 4/21/17.
+//  Copyright © 2017 Cao Yen Ngoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
